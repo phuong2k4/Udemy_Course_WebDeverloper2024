a80eefcd83d6b8afef4cf9a833a313fda08427a9
phuong2k4 <phuongsepfg@gmail.com>
2024-06-18 17:51:05 +0700

    JQueryF

---
+++ JQuery/script.js
@@ -0,0 +1,6 @@
+// use if we put script in tag head file html 
+$(document).ready(function(){
+    $("h1").css("color","yellow")
+})
+// use if we put script below the tag body in file html
+$("h1").css("color","yellow")
