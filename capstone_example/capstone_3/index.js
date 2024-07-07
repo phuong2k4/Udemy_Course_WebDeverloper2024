a87bd6e5ff7bea79d7de79023be710d62e087bf7
phuong2k4 <phuongsepfg@gmail.com>
2024-07-07 14:39:48 +0700

    CompleteCapstone3

--- capstone_example/capstone_3/index.js
+++ capstone_example/capstone_3/index.js
@@ -1,16 +1,22 @@
 import express from "express";
 import bodyParser from "body-parser";
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+import { url } from "inspector";
 
 const app = express();
-const port = 3000;
+const port = 3000
+const __dirname = dirname(fileURLToPath(import.meta.url))
 
 var email = "User"
 const func = {
     name: email
 }
 
+
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"))
+
 
 app.get("/",(req,res)=>{
     
@@ -35,11 +41,21 @@ app.post("/submit", (req,res)=>{
     res.render("index.ejs", param)
 })
 
-
 app.get("/login",(req,res)=>{
     res.render("login.ejs")
 })
 
+
+// about page
+app.get("/abouts/siddhartha.html",(req,res)=>{
+    res.sendFile(__dirname + "/views/abouts/siddhartha.html")
+})
+app.get("/abouts/anna.html",(req,res)=>{
+    res.sendFile(__dirname + "/views/abouts/anna.html")
+})
+app.get("/abouts/nhatthuc.html",(req,res)=>{
+    res.sendFile(__dirname + "/views/abouts/nhatthuc.html")
+})
 app.listen(port,()=>{
     console.log(`Server running on port ${port}`)
 })
