984cd2c954843c195bf52c061c2ce078daa66aa3
phuong2k4 <phuongsepfg@gmail.com>
2024-08-05 21:06:24 +0700

    backend-capstone5

--- capstone_example/capstone_5/index.js
+++ capstone_example/capstone_5/index.js
@@ -17,11 +17,36 @@ db.connect();
 
 app.use(bodyParser.urlencoded({extended:true}))
 app.use(express.static("public"))
+const url_book = "https://covers.openlibrary.org/b"
 
 app.get("/", async (req, res)=>{
+    res.render("index.ejs")
+})
+
+
+app.post("/submit", async (req,res)=>{
+    const value = req.body.value;
+    const key = req.body.key;
+    try{
+        const data = await axios.get(`${url_book}/${key}/${value}-L.jpg`);
+        res.render("submit.ejs", {
+            img: `${url_book}/${key}/${value}-S.jpg`
+        })
+    }catch(err){
+        console.log("Can't get api data from this url. Try again!", err.message);
+    }
+    res.render("submit.ejs")
+})
+
+
+app.post("/add",(req,res)=>{
     res.render("card-storage.ejs")
 })
 
+app.get("/note", (req,res)=>{
+    console.log("got clicked")
+    res.render("book-content.ejs")
+})
 app.listen(port,()=>{
     console.log(`Running on port http://localhost:${port}`)
 })
