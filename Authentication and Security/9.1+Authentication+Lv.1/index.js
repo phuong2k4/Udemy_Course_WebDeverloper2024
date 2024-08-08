89235aa09b93c01a0404cbdbfe402be5a6c8b56b
phuong2k4 <phuongsepfg@gmail.com>
2024-08-08 21:36:54 +0700

    complete 9.1 authASec

--- Authentication and Security/9.1+Authentication+Lv.1/index.js
+++ Authentication and Security/9.1+Authentication+Lv.1/index.js
@@ -6,13 +6,14 @@ const app = express();
 const port = 3000;
 const db = new pg.Client({
   host: 'localhost',
-  database:'',
+  database:'secret',
   user: 'postgres',
   password: '0',
   port: 5432
 })
 
 db.connect();
+
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
@@ -28,10 +29,48 @@ app.get("/register", (req, res) => {
   res.render("register.ejs");
 });
 
-app.post("/register", async (req, res) => {});
+app.post("/register", async (req, res) => {
+  const email = req.body.username;
+  const password = req.body.password;
+  try{
+    const extract_db = await db.query("select * from auth");
+    extract_db.rows.forEach( async (user)=>{
+      if (email===user.email_user){
+        res.send("Email already exists... Try another email or Wanna log in ?")
+      }else {
+        try{
+          const add_data = await db.query("Insert into auth(email_user, password_user) values ($1,$2)", [
+            email,
+            password
+          ])
+          res.render("secrets.ejs");  
+        }
+      catch(err){
+        console.log("Error query database...", err.message)
+      }}
+  })}catch(error){
+    console.log("Query cant continue, something wrong...", error.message)
+  }
+});
 
-app.post("/login", async (req, res) => {});
+app.post("/login", async (req, res) => {
+  const email = req.body.username;
+  const password = req.body.password;
+  try{
+    const auth_data = await db.query("select * from auth")
+    auth_data.rows.forEach( (user) => {
+      if(email===user.email_user && password === user.password_user){
+        res.render("secrets.ejs");
+      }else{
+        res.send("Wrong email or password, try again...")
+      }
+    })
+  }catch(error){
+
+    console.log("Query cant continue, something wrong...", error.message)
+  }
+});
 
 app.listen(port, () => {
-  console.log(`Server running on port ${port}`);
+  console.log(`Server running on port http://localhost:${port}`);
 });
