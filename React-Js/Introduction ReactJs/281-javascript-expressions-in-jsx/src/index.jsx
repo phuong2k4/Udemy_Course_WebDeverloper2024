8bb1f72970fc035a480d3bcb53d46b3621116927
phuong2k4 <phuongsepfg@gmail.com>
2024-08-14 17:29:30 +0700

    complete lession 281

--- React-Js/Introduction ReactJs/281-javascript-expressions-in-jsx/src/index.jsx
+++ React-Js/Introduction ReactJs/281-javascript-expressions-in-jsx/src/index.jsx
@@ -8,3 +8,16 @@ ReactDOM.render(<h1>Hello World!</h1>, document.getElementById("root"));
 // to install the node modules and
 // npm run dev
 // to launch your react project in your browser
+
+const fname = 'David';
+const lname = 'Junnior';
+
+ReactDOM.render(<h1>Hello {fname}</h1>, document.getElementById("root"));
+
+ReactDOM.render(
+    <div>
+        <h1>Hello {fname + " " + lname}</h1>
+        <h2>Your random number: {Math.floor(Math.random()*10)}</h2>
+    </div>,
+    document.getElementById("root")
+)
