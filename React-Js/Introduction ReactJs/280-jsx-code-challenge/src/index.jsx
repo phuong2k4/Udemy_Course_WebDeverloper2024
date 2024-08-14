88fb084357bb00587c827d23107232996dbfa33c
phuong2k4 <phuongsepfg@gmail.com>
2024-08-14 17:01:47 +0700

    complete lession 280

--- React-Js/Introduction ReactJs/280-jsx-code-challenge/src/index.jsx
+++ React-Js/Introduction ReactJs/280-jsx-code-challenge/src/index.jsx
@@ -8,3 +8,19 @@
 // to install the node modules and
 // npm run dev
 // to launch your react project in your browser
+
+
+import React from "react";
+import reactDOM from "react-dom";
+
+reactDOM.render(
+  <div>
+    <h1>My Favorite song: </h1>
+    <ol>
+      <li>I'd rather pretend by Bryant Barnes</li>
+      <li>Sicko Mode by Travis Scott ft Drake</li>
+      <li>Pray for me by The Weeknd</li>
+    </ol>
+  </div>,
+  document.getElementById("root")
+);
