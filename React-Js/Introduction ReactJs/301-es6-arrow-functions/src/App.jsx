dd4b4b7fcfd5a7633c61e9999b7b5d5edce18876
phuong2k4 <phuongsepfg@gmail.com>
2024-08-21 22:13:41 +0700

    complete 301

---
+++ React-Js/Introduction ReactJs/301-es6-arrow-functions/src/App.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+import Entry from "./components/render/Entry";
+import emojipedia from "./components/data/emojipedia";
+
+// function createEntry(emojiTerm) {
+//   return (
+//     <Entry
+//       key={emojiTerm.id}
+//       emoji={emojiTerm.emoji}
+//       name={emojiTerm.name}
+//       description={emojiTerm.meaning}
+//     />
+//   );
+// }
+
+function App() {
+  return (
+    <div>
+      <h1>
+        <span>emojipedia</span>
+      </h1>
+      <dl className="dictionary">{emojipedia.map((emojiTerm)=>{
+        return (
+          <Entry 
+            key={emojiTerm.id}
+            emoji={emojiTerm.emoji}
+            name={emojiTerm.name}
+            description={emojiTerm.meaning}
+          />
+        )
+      })}</dl>
+    </div>
+  );
+}
+
+export default App;
---
+++ React-Js/Introduction ReactJs/302-keeper-app-part-2-starting/src/index.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./components/App";
+
+ReactDOM.render(<App />, document.getElementById("root"));
+
+//Challenge. Render all the notes inside notes.js as a seperate Note
+//component.
+
+// If you're running this locally in VS Code use the commands:
+// npm install
+// to install the node modules and
+// npm run dev
+// to launch your react project in your browser
